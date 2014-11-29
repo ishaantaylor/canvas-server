d8ea29536d57f809b244ac7f449d488b3da6dfe3
Chris Hafley <dch66@case.edu>
2014-11-28 21:42:52 -0500

    hahahaa

--- hub.js
+++ hub.js
@@ -319,7 +319,7 @@ function getCanvasImage(request, response, payload) {
 						fs.writeFile(file, new Buffer(images[i], "base64"), function(err) {});
 					}
 				}
-				createCanvasImage(docs[0], fileNames);
+				xtras.createCanvasImage(docs[0], fileNames);
 
 				response.writeHead(200, {'Content-Type':'application/json'});
 				response.write(JSON.stringify(docs, 0, 4));
