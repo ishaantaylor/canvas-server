d9a27c34696112116dada68204dea8316320eaba
Chris Hafley <dch66@case.edu>
2014-11-28 23:07:10 -0500

    adhawodwowajoidwoiawj

--- hub.js
+++ hub.js
@@ -309,7 +309,9 @@ function getCanvasImage(request, response, payload) {
 				var fileNames = [];
 				fs.readdir(filePrefix, function(err, dirs) {
 					if(err){
-						fs.mkdir(filePrefix, 777, function(err){});
+						fs.mkdir(filePrefix, 777, function(err){
+							console.log("HAAAALP:::::"+err);
+						});
 					}
 				});
 				for (var i = 0; i < images.length; i++) {
