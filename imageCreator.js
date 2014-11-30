f60ebb3232a27c74e712a3e52e1509f90a72460e
Chris Hafley <dch66@case.edu>
2014-11-30 13:27:54 -0500

    hi

--- imageCreator.js
+++ imageCreator.js
@@ -60,6 +60,7 @@ function calculateCanvasImagePositions(database_ip, response, users, isPortrait,
 							"posArray" : posArray
 						});
 						makeFile(folder + "/" +"canvas.html", html);
+						console.log("Here's the html \n\n\t\t" + html);
 						
 						response.writeHead(200, {'Content-Type':'application/json'});
 						response.write(JSON.stringify(docs, 0, 4));
